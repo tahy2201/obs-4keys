d89208d41af8c4cd211d1c12883c61cf534f4d91
tahy2201 <hyg121arx8@gmail.com>
2025-05-26 23:04:21 +0900

    prisma更新用オブジェクトへの変換を切り出し、syncロジックの変更

--- scripts/lib/relatedObjects.ts
+++ scripts/lib/relatedObjects.ts
@@ -1,3 +1,24 @@
+export function generateRepositoryUpsertParams(repo: { githubId: number, name: string, owner: string, htmlUrl: string }) {
+  const repoCommonCulumns = {
+    githubId: repo.githubId,
+    name: repo.name,
+    owner: repo.owner,
+    htmlUrl: repo.htmlUrl,
+    lastSync: new Date(),
+  }
+  return {
+    where: {
+      githubId: repo.githubId,
+    },
+    update: {
+      ...repoCommonCulumns,
+    },
+    create: {
+      ...repoCommonCulumns,
+    }
+  };
+}
+
 export function generateUserUpsertParams(user: { githubId: number, login: string, avatarUrl: string, htmlUrl: string }) {
   const userCommonCulumns = {
     githubId: user.githubId,
@@ -19,25 +40,55 @@ export function generateUserUpsertParams(user: { githubId: number, login: string
   };
 }
 
-export function generateRepositoryUpsertParams(repo: {githubId: number, name: string, owner: string, htmlUrl: string}) {
-  const repoCommonCulumns = {
-    githubId: repo.githubId,
-    name: repo.name,
-    owner: repo.owner,
-    htmlUrl: repo.htmlUrl
-  }
+export function generatePRAssigneeUpsertParams(pullRequestId: number, assignee: { assigneeId: number, assigned_at: string }) {
   return {
     where: {
-      githubId: repo.githubId,
+      id: pullRequestId,
     },
-    update: {
-      ...repoCommonCulumns,
-    },
-    create: {
-      ...repoCommonCulumns,
+    data: {
+      assignees: {
+        connectOrCreate: {
+          where: {
+            pullRequestId_userId: {
+              pullRequestId: pullRequestId,
+              userId: assignee.assigneeId,
+            }
+          },
+          create: {
+            // pullRequestIdはwhereで設定されているので不要
+            userId: assignee.assigneeId,
+            assignedAt: new Date(assignee.assigned_at),
+          }
+        }
+      }
     }
-  };
+  }
 }
+
+export function generatePRReviewersUpsertParams(pullRequestId: number, reviewer: { userId: number, requested_at: string }) {
+  return {
+    where: {
+      id: pullRequestId,
+    },
+    data: {
+      requestedReviewers: {
+        connectOrCreate: {
+          where: {
+            pullRequestId_userId: {
+              pullRequestId: pullRequestId,
+              userId: reviewer.userId,
+            }
+          },
+          create: {
+            // pullRequestIdはwhereで設定されているので不要
+            userId: reviewer.userId,
+            requestedAt: new Date(reviewer.requested_at),
+          }
+        }
+      }
+    }
+  }
+};
 
 export function generateLabelUpsertParams(label: { githubId: number, name: string, color: string, description: string }) {
   const labelCommonCulumns = {
@@ -58,4 +109,28 @@ export function generateLabelUpsertParams(label: { githubId: number, name: strin
       ...labelCommonCulumns,
     }
   };
+}
+
+export function generatePRLabelUpsertParams(pullRequestId: number, labelId: number) {
+  return {
+    where: {
+      id: pullRequestId,
+    },
+    data: {
+      labels: {
+        connectOrCreate: {
+          where: {
+            pullRequestId_labelId: {
+              pullRequestId: pullRequestId,
+              labelId: labelId,
+            }
+          },
+          create: {
+            // pullRequestIdはwhereで設定されているので不要
+            labelId: labelId
+          }
+        }
+      }
+    }
+  }
 }
--- scripts/sync-github-data.ts
+++ scripts/sync-github-data.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { Octokit } from 'octokit';
 import logger from '../logging.js'
 import { execApiWithRetry } from './utils.js';
-import { generateLabelUpsertParams, generateRepositoryUpsertParams, generateUserUpsertParams } from './lib/relatedObjects';
+import { generateLabelUpsertParams, generatePRAssigneeUpsertParams, generatePRLabelUpsertParams, generatePRReviewersUpsertParams, generateRepositoryUpsertParams, generateUserUpsertParams } from './lib/relatedObjects';
 
 dotenv.config({ path: '.env.local' }); // .env.local を読み込む場合
 
@@ -40,6 +40,7 @@ async function main() {
       },
       select: {
         id: true,
+        lastSync: true,
       }
     });
     if (!repository) {
@@ -62,7 +63,7 @@ async function main() {
     logger.info(`Repository Owner: ${REPO_OWNER}`);
 
     //1. プルリクエスト取得
-    await syncPullRequests(repository.id);
+    await syncPullRequests(repository.id, repository.lastSync);
     //await getSamplePullRequest();
 
 
@@ -79,246 +80,178 @@ async function main() {
   }
 }
 
-async function syncPullRequests(repositoryId: number) {
-  // DBから、リポジトリの中でnumberが最大のPRを取得
-  const maxPr = await prisma.pullRequest.findFirst({
+async function syncPullRequests(repositoryId: number, lastSync: Date) {
+  // ループ
+  for (let page = 1; ; page++) {
+    const { data: pullRequests } = await octokit.rest.pulls.list({
+      owner: REPO_OWNER,
+      repo: REPO_NAME,
+      state: 'all',
+      per_page: 100,
+      page,
+      sort: 'updated',
+      direction: 'desc',
+    });
+
+    for (const pr of pullRequests) {
+      // 最後の同期日時以前に更新されたプルリクエストが見つかったら処理終了
+      if (new Date(pr.updated_at) <= lastSync) {
+        logger.info(`No new pull requests since last sync at ${lastSync}. Stopping sync.`);
+        return;
+      }
+      if (page > 2) {
+        // サンプルのため、2ページ目以降は処理をスキップ
+        return;
+      }
+      processPullRequest(pr, repositoryId)
+    }
+  }
+}
+
+async function processPullRequest(pr: any, repositoryId: number) {
+  const {
+    number,
+    state,
+    created_at,
+    updated_at,
+    merged_at,
+    closed_at,
+    title,
+    author,
+    assignee,
+    requested_reviewers: requestedReviewers = []
+  } = pr;
+
+  // upsert
+  // PRの共通データを定義
+  // GitHubのAPIは小文字のstateを返すので、PrismaのEnumに合わせて大文字に変換
+  const convertedState = state.toUpperCase() as PullRequestState;
+
+  const prCommonData = {
+    state: convertedState,
+    title,
+    updatedAt: new Date(updated_at),
+    mergedAt: merged_at ? new Date(merged_at) : null,
+    closedAt: closed_at ? new Date(closed_at) : null,
+    baseRefName: pr.base.ref,
+    headRefName: pr.head.ref,
+    repository: {
+      connect: {
+        id: repositoryId,
+      }
+    },
+    leadTimeInSeconds: calculateLeadTimeInSeconds(created_at, merged_at)
+  };
+
+  let pullRequest = await prisma.pullRequest.upsert({
     where: {
-      repositoryId: repositoryId,
+      repositoryId_number: {
+        repositoryId: repositoryId,
+        number: number,
+      },
     },
-    orderBy: {
-      number: 'desc',
+    update: {
+      ...prCommonData,
     },
-    select: {
-      number: true,
+    create: {
+      ...prCommonData,
+      githubId: BigInt(pr.id),
+      number,
+      createdAt: new Date(created_at),
+      url: pr.html_url,
     }
   });
 
-  // 1. プルリクエストの最大IDを取得
-  const { data: latestPr } = await octokit.rest.pulls.list({
-    owner: REPO_OWNER,
-    repo: REPO_NAME,
-    state: 'all',
-    per_page: 1,
-    page: 1,
-    sort: 'created',
-    direction: 'desc',
-  });
-
-  // 取得したmaxPr と、APIから取得したPRの最大IDを比較して、差分を取得する
-  const prNumberDifference = latestPr[0].number - (maxPr ? maxPr.number : 0);
-  logger.info(`Latest PR number from API: ${latestPr[0].number}`);
-  logger.info(`PR number difference: ${prNumberDifference}`);
-  if (prNumberDifference <= 0) {
-    logger.info('No new pull requests to sync.');
-    return;
-  }
-
-  // (prNumberDifference / 30) + 1回繰り返す
-  // const perPage = 30; // 1ページあたりのPR数
-  // const repetitionNumber = Math.ceil(prNumberDifference / perPage) + 1;
-    const perPage = 5; // 1ページあたりのPR数
-    const repetitionNumber = 1;
-
-  for (let i = 1; i <= repetitionNumber; i++) {
-    const { data: fetchedPRs } = await execApiWithRetry(
-      () => octokit.rest.pulls.list({
-        owner: REPO_OWNER,
-        repo: REPO_NAME,
-        state: 'all',
-        per_page: perPage,
-        page: i,
-        sort: 'created',
-        direction: 'desc',
+  // author
+  if (author) {
+    const authorUser = await prisma.user.upsert(
+      generateUserUpsertParams({
+        githubId: author.id,
+        login: author.login,
+        avatarUrl: author.avatar_url,
+        htmlUrl: author.html_url
       })
     );
 
-    for (const pr of fetchedPRs) {
-      const {
-        number,
-        state,
-        created_at,
-        updated_at,
-        merged_at,
-        closed_at,
-        title,
-        author,
-        assignee,
-        requested_reviewers: requestedReviewers = []
-       } = pr;
-
-      // upsert
-      // PRの共通データを定義
-      // GitHubのAPIは小文字のstateを返すので、PrismaのEnumに合わせて大文字に変換
-      const convertedState = state.toUpperCase() as PullRequestState;
-      
-      const prCommonData = {
-        state: convertedState,
-        title,
-        updatedAt: new Date(updated_at),
-        mergedAt: merged_at ? new Date(merged_at) : null,
-        closedAt: closed_at ? new Date(closed_at) : null,
-        baseRefName: pr.base.ref,
-        headRefName: pr.head.ref,
-        repository: {
-          connect: {
-            id: repositoryId,
-          }
-        },
-        leadTimeInSeconds: calculateLeadTimeInSeconds(created_at, merged_at)
-      };
-
-      let pullRequest = await prisma.pullRequest.upsert({
-        where: {
-          repositoryId_number: {
-            repositoryId: repositoryId,
-            number: number,
-          },
-        },
-        update: {
-          ...prCommonData,
-        },
-        create: {
-          ...prCommonData,
-          githubId: BigInt(pr.id),
-          number,
-          createdAt: new Date(created_at),
-          url: pr.html_url,
-        }
-      });
-
-      // author
-      if (author) {
-        const authorUser = await prisma.user.upsert(
-          generateUserUpsertParams({
-            githubId: author.id,
-            login: author.login,
-            avatarUrl: author.avatar_url,
-            htmlUrl: author.html_url
-          })
-        );
-
-        await prisma.pullRequest.update({
-          where: {
-            id: pullRequest.id,
-          },
-          data: {
-            authorId: authorUser.id,
-          }
-        });
+    await prisma.pullRequest.update({
+      where: {
+        id: pullRequest.id,
+      },
+      data: {
+        authorId: authorUser.id,
       }
+    });
+  }
 
-      // labels
-      if (pr.labels && pr.labels.length > 0) {
-        for (const l of pr.labels) {
-          const label = await prisma.label.upsert(
-            generateLabelUpsertParams({
-              githubId: l.id,
-              name: l.name,
-              color: l.color,
-              description: l.description || ''
-            })
-          );
-          await prisma.pullRequest.update({
-            where: {
-              id: pullRequest.id,
-            },
-            data: {
-              labels: {
-                connectOrCreate: {
-                  where: {
-                    pullRequestId_labelId: {
-                      pullRequestId: pullRequest.id,
-                      labelId: label.id,
-                    }
-                  },
-                  create: {
-                    // pullRequestIdはwhereで設定されているので不要
-                    labelId: label.id
-                  }
-                }
-              }
-            }
-          });
-        }
-      }
-
-      // assignee
-      if (assignee) {
-        const assigneeUser = await prisma.user.upsert(
-          generateUserUpsertParams(
-            {
-              githubId: assignee.id,
-              login: assignee.login,
-              avatarUrl: assignee.avatar_url,
-              htmlUrl: assignee.html_url
-            }
-          )
-        );
-
-        await prisma.pullRequest.update({
-          where: {
-            id: pullRequest.id,
-          },
-          data: {
-            assignees: {
-              connectOrCreate: {
-                where: {
-                  pullRequestId_userId: {
-                    pullRequestId: pullRequest.id,
-                    userId: assigneeUser.id,
-                  }
-                },
-                create: {
-                  // pullRequestIdはwhereで設定されているので不要
-                  userId: assigneeUser.id,
-                  assignedAt: new Date(assignee.assigned_at),
-                }
-              }
-            }
-          }
-        });
-      }
-
-      // requestedReviewers
-      if (requestedReviewers.length > 0) {
-        for (const reviewer of requestedReviewers) {
-          const reviewerUser = await prisma.user.upsert(
-            generateUserUpsertParams({
-              githubId: reviewer.id,
-              login: reviewer.login,
-              avatarUrl: reviewer.avatar_url,
-              htmlUrl: reviewer.html_url
-            })
-          );
-
-          await prisma.pullRequest.update({
-            where: {
-              id: pullRequest.id,
-            },
-            data: {
-              requestedReviewers: {
-                connectOrCreate: {
-                  where: {
-                    pullRequestId_userId: {
-                      pullRequestId: pullRequest.id,
-                      userId: reviewerUser.id,
-                    }
-                  },
-                  create: {
-                    // pullRequestIdはwhereで設定されているので不要
-                    userId: reviewerUser.id,
-                    requestedAt: new Date(reviewer.requested_at),
-                  }
-                }
-              }
-            }
-          });
-        }
-      }
-
-      // TODO: reviews
+  // labels
+  if (pr.labels && pr.labels.length > 0) {
+    for (const l of pr.labels) {
+      const label = await prisma.label.upsert(
+        generateLabelUpsertParams({
+          githubId: l.id,
+          name: l.name,
+          color: l.color,
+          description: l.description || ''
+        })
+      );
+      await prisma.pullRequest.update(
+        generatePRLabelUpsertParams(
+          pullRequest.id,
+          label.id
+        )
+      );
     }
   }
+
+  // assignee
+  if (assignee) {
+    const assigneeUser = await prisma.user.upsert(
+      generateUserUpsertParams(
+        {
+          githubId: assignee.id,
+          login: assignee.login,
+          avatarUrl: assignee.avatar_url,
+          htmlUrl: assignee.html_url
+        }
+      )
+    );
+
+    await prisma.pullRequest.update(
+      generatePRAssigneeUpsertParams(
+        pullRequest.id,
+        { 
+          assigneeId: assigneeUser.id,
+          assigned_at: pr.assigned_at || new Date().toISOString() 
+        }
+      )
+    );
+  }
+
+  // requestedReviewers
+  if (requestedReviewers.length > 0) {
+    for (const reviewer of requestedReviewers) {
+      const reviewerUser = await prisma.user.upsert(
+        generateUserUpsertParams({
+          githubId: reviewer.id,
+          login: reviewer.login,
+          avatarUrl: reviewer.avatar_url,
+          htmlUrl: reviewer.html_url
+        })
+      );
+
+      await prisma.pullRequest.update(
+        generatePRReviewersUpsertParams(
+          pullRequest.id,
+          {
+            userId: reviewerUser.id,
+            requested_at: reviewer.requested_at || new Date().toISOString()
+          }
+        )
+      );
+    }
+  }
+
+  // TODO: reviews
 }
 
 async function getSamplePullRequest() {
